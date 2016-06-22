cb93ee4ece13743abe7a7a5647477365a9e5113b
JeremyKing <jgroups@163.com>
2016-06-23 00:04:58 +0800

    java heap oom

--- src/me/lbing/oom/HeapOOM.java
+++ src/me/lbing/oom/HeapOOM.java
@@ -3,6 +3,11 @@ package me.lbing.oom;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * VM Args: -Xms20m -Xmx20m -XX:+HeapDumpOnOutOfMemoryError
+ * 
+ * @author king
+ */
 public class HeapOOM {
 	static class OOMObject {
 
@@ -15,3 +20,18 @@ public class HeapOOM {
 		}
 	}
 }
+/**********运行结果**********/
+/*
+java.lang.OutOfMemoryError: Java heap space
+Dumping heap to java_pid671.hprof ...
+Heap dump file created [27633188 bytes in 0.123 secs]
+Exception in thread "main" java.lang.OutOfMemoryError: Java heap space
+	at java.util.Arrays.copyOf(Arrays.java:3210)
+	at java.util.Arrays.copyOf(Arrays.java:3181)
+	at java.util.ArrayList.grow(ArrayList.java:261)
+	at java.util.ArrayList.ensureExplicitCapacity(ArrayList.java:235)
+	at java.util.ArrayList.ensureCapacityInternal(ArrayList.java:227)
+	at java.util.ArrayList.add(ArrayList.java:458)
+	at me.lbing.oom.HeapOOM.main(HeapOOM.java:18)
+*/
+
