534611f90a0560f72df03903dace7773e04e54cf
JeremyKing <jgroups@163.com>
2016-06-24 10:59:00 +0800

    -XX:PretenureSizeThreshold参数 令大于这个这个设置值的对象直接在老年代分配 只对Serial和ParNew收集器有效

--- src/me/lbing/allocation/Eden.java
+++ src/me/lbing/allocation/Eden.java
@@ -4,7 +4,7 @@ public class Eden {
 	private static final int _1MB = 1024 * 1024;
 
 	/**
-	 * VM参数:-verbose:gc -Xmx20M -Xms20M -Xmn10M -XX:+PrintGCDetails -XX:SurvivorRatio:8
+	 * VM参数:-verbose:gc -Xmx20M -Xms20M -Xmn10M -XX:+PrintGCDetails -XX:SurvivorRatio=8
 	 */
 	public static void testAllocation() {
 		byte[] allocation1, allocation2, allocation3, allocation4;
@@ -30,7 +30,7 @@ ParOldGen       total 10240K, used 4096K [0x00000000fec00000, 0x00000000ff600000
 Metaspace       used 2650K, capacity 4486K, committed 4864K, reserved 1056768K
  class space    used 286K, capacity 386K, committed 512K, reserved 1048576K
  */
-/**********运行结果jdk1.8.0_91**********/
+/**********运行结果jdk1.7.0_79**********/
 /*
 Heap
  PSYoungGen      total 9216K, used 7143K [0x00000000ff600000, 0x0000000100000000, 0x0000000100000000)
---
+++ src/me/lbing/allocation/Tenure.java
@@ -0,0 +1,30 @@
+package me.lbing.allocation;
+
+public class Tenure {
+	private static final int _1MB = 1024 * 1024;
+
+	/**
+	 * VM参数:-verbose:gc -Xmx20M -Xms20M -Xmn10M -XX:+PrintGCDetails -XX:SurvivorRatio=8
+	 * -XX:PretenureSizeThreshold=3145728
+	 */
+	public static void testPretenureSizeThreshold() {
+		byte[] allocation;
+		allocation = new byte[4 * _1MB];// 出现一次Minor GC
+	}
+	
+	public static void main(String[] args) {
+		testPretenureSizeThreshold();
+	}
+}
+/**********运行结果jdk1.7.0_79**********/
+/*
+Heap
+ PSYoungGen      total 9216K, used 5095K [0x00000000ff600000, 0x0000000100000000, 0x0000000100000000)
+  eden space 8192K, 62% used [0x00000000ff600000,0x00000000ffaf9f88,0x00000000ffe00000)
+  from space 1024K, 0% used [0x00000000fff00000,0x00000000fff00000,0x0000000100000000)
+  to   space 1024K, 0% used [0x00000000ffe00000,0x00000000ffe00000,0x00000000fff00000)
+ ParOldGen       total 10240K, used 0K [0x00000000fec00000, 0x00000000ff600000, 0x00000000ff600000)
+  object space 10240K, 0% used [0x00000000fec00000,0x00000000fec00000,0x00000000ff600000)
+ PSPermGen       total 21504K, used 2569K [0x00000000f9a00000, 0x00000000faf00000, 0x00000000fec00000)
+  object space 21504K, 11% used [0x00000000f9a00000,0x00000000f9c827f8,0x00000000faf00000)
+ */
